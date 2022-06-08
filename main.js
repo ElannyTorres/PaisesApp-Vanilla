b4d489cec426fa1bb27f7306459255cd41580beb
Elanny Torres <torres.elanny@gmail.com>
2022-06-08 08:28:47 -0500

    función fetch y creación de cards

---
+++ main.js
@@ -0,0 +1,17 @@
+import createPaisCard from './components/Card/card.js';
+
+/* API RESTCOUNTRY */
+const paisesContainer = document.querySelector('.paisesContainer');
+
+const Pais_URL = 'https://restcountries.com/v3.1/lang/spa';
+
+fetch(Pais_URL)
+  .then((res) => res.json())
+  .then((resJson) => {
+    const newResJson = resJson.splice(0, 12);
+    for (let pais of newResJson) {
+      createPaisCard(pais, paisesContainer);
+    }
+  });
+
+/* MODAL FUNCTION */
