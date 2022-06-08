6932bbbf030ab8550898cec1bb483be36acf1362
Elanny Torres <torres.elanny@gmail.com>
2022-06-08 11:00:47 -0500

    agregar atributo

--- components/Modal/modal.js
+++ components/Modal/modal.js
@@ -1,11 +1,12 @@
 export const createModal = (pais, paisID, container) => {
   const modal = document.createElement('div');
+  modal.setAttribute('class', 'modalContainer');
   modal.innerHTML = `
-      <div class="modalContainer" id=${paisID.toLowerCase()}>
-        <h3>${pais.name.common} se encuentra en:</h3>
-        <p>${pais.continents}</p>
-        <button class="close-btn">X</button>
-      </div>
+    <div id=${paisID.toLowerCase()}>
+      <h3>${pais.name.common} se encuentra en:</h3>
+      <p>${pais.continents}</p>
+      <button class="close-btn">Cerrar</button>
+    </div>
   `;
   container.appendChild(modal);
 };
