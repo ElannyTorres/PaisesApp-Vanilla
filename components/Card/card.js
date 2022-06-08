c3b22c58779edc82a79286544c563f6b5cdceb2b
Elanny Torres <torres.elanny@gmail.com>
2022-06-08 08:29:23 -0500

    componente card y funcion para crear dinámicamente cada una

---
+++ components/Card/card.js
@@ -0,0 +1,29 @@
+function separator(numb) {
+  var str = numb.toString().split('.');
+  str[0] = str[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return str.join('.');
+}
+
+const createPaisCard = (pais, container) => {
+  const paisInfo = document.createElement('div');
+  const paisID = pais.fifa || '';
+  paisInfo.innerHTML = `
+    <div class="paisContainer">
+      <img class="flags" src=${pais.flags.png} alt="" />
+      <h3>${pais.name.common}</h3>
+      <p><strong>Capital:</strong> ${pais.capital}</p>
+      <p><strong>Population:</strong> ${separator(pais.population)}</p>
+      <button>Ver más</button>
+    </div>
+    <div class="modalOverlay">
+      <div class="modalContainer" id=${paisID.toLowerCase()}>
+        <h3>${pais.name.common} se encuentra en:</h3>
+        <p>${pais.continents}</p>
+        <button class="close-btn">X</button>
+      </div>
+    </div>
+  `;
+  container.appendChild(paisInfo);
+};
+
+export default createPaisCard;
